8e689d68f29f92eba1ac63e6df96dc3a927de5e9
Alexandre Leblanc <alexandreleblanc89@gmail.com>
2021-02-25 15:38:37 -0500

    clean up secrets

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,9 +3,4 @@ module.exports = {
     USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PASS,
     DB: process.env.DB_DATABASE
-
-    // HOST: "coding-challenge.cmsf3iywk911.us-east-1.rds.amazonaws.com",
-    // USER: "cptnwookie",
-    // PASSWORD: "056051al",
-    // DB: "coding-challenge"
-  };
+};
