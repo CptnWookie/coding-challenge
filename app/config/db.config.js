97b3278743637841ab898ab0da79110fa5e18af1
Alexandre Leblanc <alexandreleblanc89@gmail.com>
2021-02-25 12:26:47 -0500

    updating env vars

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    // HOST: process.env.DB_HOST,
-    // USER: process.env.DB_USER,
-    // PASSWORD: process.env.DB_PASS,
-    // DB: process.env.DB_DATABASE
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASS,
+    DB: process.env.DB_DATABASE
 
-    HOST: "coding-challenge.cmsf3iywk911.us-east-1.rds.amazonaws.com",
-    USER: "cptnwookie",
-    PASSWORD: "056051al",
-    DB: "coding-challenge"
+    // HOST: "coding-challenge.cmsf3iywk911.us-east-1.rds.amazonaws.com",
+    // USER: "cptnwookie",
+    // PASSWORD: "056051al",
+    // DB: "coding-challenge"
   };
