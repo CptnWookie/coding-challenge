d88a1cf548409836c16e9f4b6597382b3f14137e
Alexandre Leblanc <alexandreleblanc89@gmail.com>
2021-02-25 18:00:33 -0500

    trying to fix CORS

--- server.js
+++ server.js
@@ -27,8 +27,21 @@ app.get("/", (req, res) => {
 
 require("./app/routes/techno.routes.js")(app);
 
-const PORT = process.env.PORT || 3000;
-// set port, listen for requests
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
+
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
+// const PORT = process.env.PORT || 3000;
+// // set port, listen for requests
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}.`);
+// });
