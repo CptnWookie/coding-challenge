b636636f4b9064864629c2f481e8955d869e9e6d
Alexandre Leblanc <alexandreleblanc89@gmail.com>
2021-02-26 04:00:52 -0500

    final

--- app/controllers/techno.controller.js
+++ app/controllers/techno.controller.js
@@ -18,34 +18,34 @@ exports.findOne = (req, res) => {
     });
   };
 
-// Retrieve all Technos from the database.
+// Ninjify Endpoint
 exports.findAll = (req, res) => {
-    var querybuzzwords = req.query.x;
-    var wordArray = querybuzzwords.split(",");
+    var querybuzzwords = req.query.x;  //Requested Buzzword for the query
+    var wordArray = querybuzzwords.split(","); //Split querybuzzwords string into an Array
 
     getNinjaWords(wordArray, function(ninjaString) {
-      if (ninjaString != null  && ninjaString != ""){
+      if (ninjaString != null  && ninjaString != ""){  //If The value submitted aren't null or empty, the response is sent in the callback message.
         res.status(200).send({
           message: ninjaString
         });
       }
       else{
-        res.status(404).send({
-          message: "No match found."
+        res.status(404).send({            //Otherwise, the "No Match Found" message is sent.
+          message: "No Match Found."
         });
       }
     });
   };
 
   function getByBuzzFunc(providedBuzz, callback) {
-     sql.query(`SELECT ninja FROM techno WHERE buzz in (${providedBuzz})`, function (err, result) {
+     sql.query(`SELECT ninja FROM techno WHERE buzz in (${providedBuzz})`, function (err, result) { // Select the correspondant Ninja word in the database for each Buzzword in the query.
       if (err) result = new Array();
 
       callback(result);
     });
   }
 
-  function getNinjaWords(providedWordArray, callback) {
+  function getNinjaWords(providedWordArray, callback) {  
     var sqlConditionString = "";
 
     if (providedWordArray.length == 1){
