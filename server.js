eb3c7574c2be65c363ce354fb441c0a7b421e033
Alexandre Leblanc <alexandreleblanc89@gmail.com>
2021-02-25 19:39:33 -0500

    trying to fix CORS

--- server.js
+++ server.js
@@ -4,15 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(cors())
-
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
+app.use(cors());
 
 // parse requests of content-type: application/json
 app.use(bodyParser.json());
@@ -27,21 +19,8 @@ app.get("/", (req, res) => {
 
 require("./app/routes/techno.routes.js")(app);
 
-// Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '0.0.0.0';
-// Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
-
-var cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
+const PORT = process.env.PORT || 3000;
+// set port, listen for requests
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
 });
-// const PORT = process.env.PORT || 3000;
-// // set port, listen for requests
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}.`);
-// });
